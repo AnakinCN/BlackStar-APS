abc0c9bc44fa2ef4826cf97c2e95831675d67ba9
AnakinCN <anakincn@sina.com>
2020-12-11 12:02:24 +0800

    almost ok

--- Samples/HelloBlackStar/HelloBlackStar/Program.cs
+++ Samples/HelloBlackStar/HelloBlackStar/Program.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using BlackStar;
 using BlackStar.Model;
 using BlackStar.Algorithms;
+using BlackStar.USL;
+using System.Xml.Schema;
 
 namespace HelloBlackStar
 {
@@ -11,21 +13,44 @@ namespace HelloBlackStar
         static void Main(string[] args)
         {
             Console.WriteLine("Hello BlackStar-APS!");
+            
+            //Console.ReadLine();
             EnvModel.InitializeEnvModel();
             Console.WriteLine(LicensingOP.LicenseInfo);
-            CreateResources();      //创建资源
+
+            USLManagerOP.InitializeUSLOP();
+            USLManagerOP.dsUSL.ReadXml("default.usl");
+            
+            CreateModel();  //创建默认型号
+            CreateResourcesServices();      //创建资源服务原型
             DataSetBlackStar.dtSampleCaseRow samplcase = CreateSampleCase();     //创建算例
             CreateAvailability();   //创建可用性
             CreateAction();         //创建动作需求
-            Solve(samplcase);
+            Solve(samplcase);       //求解安排动作
+        }
+
+        private static void CreateModel()
+        {
+            DataSetBlackStar.dtModelRow model = EnvModel.dsBlackStar.dtModel.NewdtModelRow();
+            model.型号族 = "车队";
+            model.启用 = true;
+            EnvModel.dsBlackStar.dtModel.AdddtModelRow(model);
+
+            DataSetBlackStar.dtModelConfigRow config = EnvModel.dsBlackStar.dtModelConfig.NewdtModelConfigRow();
+            config.型号族 = "车队";
+            config.参数名 = "特征点正则表达式";
+            config.参数值 = @"T[a-z_]*\d*";
+            EnvModel.dsBlackStar.dtModelConfig.AdddtModelConfigRow(config);
+
+            EnvModel.CurrentModel= "车队";
         }
 
         private static void Solve(DataSetBlackStar.dtSampleCaseRow samplcase)
         {
             bool result = AlgorithmOP.ArrangeActionSimple(
                 samplcase,
-                "dilivery",
-                new DateTime(2020, 1, 20, 7, 0, 0),
+                "delivery",
+                new DateTime(2020, 1, 1, 0, 0, 0),
                  AlgorithmOP.PostponeOption.后移,
                  TimeSpan.FromDays(1),
                  false,
@@ -34,10 +59,10 @@ namespace HelloBlackStar
                  "",
                  ""
                 );
-            Console.WriteLine("安排成功" + result.ToString());
-            foreach(DataSetSampleCase.dtEventRow eventrow in samplcase.dsSampleCase.dtEvent.Rows)
+            Console.WriteLine("安排成功：" + result.ToString());
+            foreach (DataSetSampleCase.dtEventRow eventrow in samplcase.dsSampleCase.dtEvent.Rows)
             {
-                Console.WriteLine($"{eventrow.事件名称} {eventrow.事件代号} {eventrow.开始.ToString()}");
+                Console.WriteLine($"{eventrow.事件名称} {eventrow.事件代号} {eventrow.开始} {eventrow.结束}");
             }
         }
 
@@ -47,23 +72,29 @@ namespace HelloBlackStar
             DataSetBlackStar.dtActionCategoryRow category = EnvModel.dsBlackStar.dtActionCategory.NewdtActionCategoryRow();
             category.终结动作 = true;
             category.动作类别 = "默认动作类别";
+            category.动作代号模式 = @"\S+";
             EnvModel.dsBlackStar.dtActionCategory.AdddtActionCategoryRow(category);
 
             DataSetBlackStar.dtActionCategoryUnityRow unity = EnvModel.dsBlackStar.dtActionCategoryUnity.NewdtActionCategoryUnityRow();
-            unity.公共元素表达式 = "t ：consume carry 300sec";
+            //unity.公共元素表达式 = "(t ：consume large1 carry 20sec) 或 (t ：consume small1 carry 20sec) ";
+            unity.公共元素表达式 = "t ：consume carry 20sec";
+            unity.动作类别 = "默认动作类别";
             EnvModel.dsBlackStar.dtActionCategoryUnity.AdddtActionCategoryUnityRow(unity);
 
             //创建一个动作
             DataSetBlackStar.dtActionRow action = EnvModel.dsBlackStar.dtAction.NewdtActionRow();
-            action.动作类别= "默认动作类别";
+            action.动作类别 = "默认动作类别";
             action.动作名称 = "运送";
-            action.动作代号 = "dilivery";
+            action.动作代号 = "delivery";
             EnvModel.dsBlackStar.dtAction.AdddtActionRow(action);
             //创建动作的具体步骤
             DataSetBlackStar.dtActionElementRow element = EnvModel.dsBlackStar.dtActionElement.NewdtActionElementRow();
-            element.动作代号= "dilivery";
+            element.动作代号 = "delivery";
+            element.元素代号 = "delivery";
+            element.元素表达式 = "";
             element.顺序 = 1;
             element.约束 = "t";
+            element.持续 = "20s";
             EnvModel.dsBlackStar.dtActionElement.AdddtActionElementRow(element);
         }
 
@@ -78,7 +109,7 @@ namespace HelloBlackStar
             return sample;
         }
 
-        private static void CreateResources()
+        private static void CreateResourcesServices()
         {
 
             //创建车辆资源类别
@@ -97,19 +128,34 @@ namespace HelloBlackStar
             resource2.资源类别 = "车辆";
             EnvModel.dsBlackStar.dtResource.AdddtResourceRow(resource1);
             EnvModel.dsBlackStar.dtResource.AdddtResourceRow(resource2);
+
+            //创建服务种类
+            DataSetBlackStar.dtServiceCategoryRow servcieCategory = EnvModel.dsBlackStar.dtServiceCategory.NewdtServiceCategoryRow();
+            servcieCategory.服务类别 = "默认服务类别";
+            EnvModel.dsBlackStar.dtServiceCategory.AdddtServiceCategoryRow(servcieCategory);
+
+            //创建服务
+            DataSetBlackStar.dtServiceRow service = EnvModel.dsBlackStar.dtService.NewdtServiceRow();
+            service.服务类别 = "默认服务类别";
+            service.服务代号 = "carry";
+            service.服务名称 = "运载";
+            EnvModel.dsBlackStar.dtService.AdddtServiceRow(service);
         }
 
         private static void CreateAvailability()    //创建可用性
         {
-            //创建可用性
+            //创建算例可用性
             DataSetBlackStar.dtSampleCaseRow sample = EnvModel.dsBlackStar.dtSampleCase.Rows.Cast<DataSetBlackStar.dtSampleCaseRow>().FirstOrDefault();
 
-            for(int i=0;i<8;i++)
+            for (int i = 0; i < 8; i++)
             {
                 DataSetSampleCase.dtAvailabilityRow avail = sample.dsSampleCase.dtAvailability.NewdtAvailabilityRow();
                 avail.可用性ID = Guid.NewGuid();
                 avail.资源代号 = "large1";
                 avail.服务代号 = "carry";
+                avail.可用性描述 = "large1 carry 1";
+                avail.可用性类型 = "常值";
+                avail.启用 = true;
                 avail.开始 = new DateTime(2020, 1, 1, 8 + i, 0, 0); //每隔1小时
                 avail.结束 = new DateTime(2020, 1, 1, 8 + i, 10, 0); //服务10分钟
                 sample.dsSampleCase.dtAvailability.AdddtAvailabilityRow(avail);
@@ -121,9 +167,17 @@ namespace HelloBlackStar
                 avail.可用性ID = Guid.NewGuid();
                 avail.资源代号 = "small1";
                 avail.服务代号 = "carry";
+                avail.可用性描述 = "small1 carry 1";
+                avail.可用性类型 = "常值";
+                avail.启用 = true;
                 avail.开始 = new DateTime(2020, 1, 1, 8 + i, 0, 0); //每隔1小时
                 avail.结束 = new DateTime(2020, 1, 1, 8 + i, 10, 0); //服务10分钟
                 sample.dsSampleCase.dtAvailability.AdddtAvailabilityRow(avail);
+            }
+
+            foreach (DataSetSampleCase.dtAvailabilityRow avail in sample.dsSampleCase.dtAvailability.Rows)
+            {
+                Console.WriteLine($"{avail.资源代号} {avail.服务代号} {avail.开始.ToString("yyyy-MM-dd HH:mm")} {avail.结束.ToString("yyyy-MM-dd HH:mm")}");
             }
         }
     }
